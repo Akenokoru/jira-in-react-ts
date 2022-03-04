33e3924c98da78a52f753b3fc5941c783cbc004d
Akenokoru <akenokoru.pan@gmail.com>
2022-03-04 10:30:46 +0800

    chore: change to ts

---
+++ src/screens/project-list/index.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import { SearchPanel } from "./search-panel";
+import { List } from "./list";
+import * as qs from "qs";
+import { cleanObject, useMount, useDebounce } from "../../utils/index";
+
+const apiUrl = process.env.REACT_APP_API_URL;
+
+export const ProjectListScreen = () => {
+  const [param, setParam] = useState({
+    name: "",
+    personId: "",
+  });
+  const debouncedParam = useDebounce(param, 200);
+
+  const [list, setList] = useState([]);
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      `${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`
+    ).then(async (response) => {
+      if (response.ok) {
+        setList(await response.json());
+      }
+    });
+  }, [debouncedParam]);
+
+  useMount(() => {
+    fetch(`${apiUrl}/users`).then(async (response) => {
+      if (response.ok) {
+        setUsers(await response.json());
+      }
+    });
+  });
+
+  return (
+    <div>
+      <SearchPanel users={users} param={param} setParam={setParam} />
+      <List list={list} users={users} />
+    </div>
+  );
+};
