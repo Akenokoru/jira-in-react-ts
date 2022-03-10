fec01074840f6f1e2f19667f9af65a9ef1afcbea
Akenokoru <akenokoru.pan@gmail.com>
2022-03-10 11:52:50 +0800

    feat: add auth provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./App.css";
 import { ProjectListScreen } from "screens/project-list";
-import { LoginScreen } from "screens/login/inedx";
+import { LoginScreen } from "screens/login";
 
 function App() {
   return (
